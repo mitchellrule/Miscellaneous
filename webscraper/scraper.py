25f691722db585bba11123975d01dbf3016286e5
Mitch Rule <mitchman5506@gmail.com>
2015-07-16 13:35:40 +1000

    Started experimenting with BS4

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -1,1 +1,7 @@
 from bs4 import BeautifulSoup
+import urllib2
+
+
+website = urllib2.urlopen("http://stats.espncricinfo.com/ci/engine/records/team/match_results.html?class=1;id=2014;type=year")
+soup = BeautifulSoup(website.read())
+
