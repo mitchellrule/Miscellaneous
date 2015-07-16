b7fe7ce61771adbfe38053134fffc52e2e81a48c
Mitch Rule <mitchman5506@gmail.com>
2015-07-16 16:23:07 +1000

    Worked on logging and recording data

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -5,3 +5,4 @@ import urllib2
 website = urllib2.urlopen("http://stats.espncricinfo.com/ci/engine/records/team/match_results.html?class=1;id=2014;type=year")
 soup = BeautifulSoup(website.read())
 
+print soup.prettify()
