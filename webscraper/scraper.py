d23c40413c1217f5fd50f792df35b4f5f57829ca
Mitch Rule <mitchman5506@gmail.com>
2015-07-17 15:07:54 +1000

    Cleaned comments

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -1,10 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-
-#website = urllib2.urlopen("https://en.wikipedia.org/wiki/Python_(programming_language)")
-#soup = BeautifulSoup(website.read())
-
 #TODO Convert to reusable functions and objects
 def getLinks():
 	website = urllib2.urlopen(raw_input('Enter website url: '))
