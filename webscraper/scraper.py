a37dfd1920d0ec615d884b9b2d2963ac0a0b01d0
Mitch Rule <mitchman5506@gmail.com>
2015-07-17 15:04:24 +1000

    Modified webscraper to use functions

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -2,7 +2,20 @@ from bs4 import BeautifulSoup
 import urllib2
 
 
-website = urllib2.urlopen("http://stats.espncricinfo.com/ci/engine/records/team/match_results.html?class=1;id=2014;type=year")
-soup = BeautifulSoup(website.read())
+#website = urllib2.urlopen("https://en.wikipedia.org/wiki/Python_(programming_language)")
+#soup = BeautifulSoup(website.read())
 
-print soup.prettify()
+#TODO Convert to reusable functions and objects
+def getLinks():
+	website = urllib2.urlopen(raw_input('Enter website url: '))
+	soup = BeautifulSoup(website.read())
+
+	log = open('log.txt', 'ab')
+
+	for link in soup.find_all('a'):
+		log.write(str(link.get('href')))
+		log.write('\n')
+		print (link.get('href'))
+	log.close()
+
+getLinks()
