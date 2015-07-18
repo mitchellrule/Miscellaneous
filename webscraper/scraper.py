9f4d39b2d96b918c18dee46ec08de6f858112a6d
Mitch Rule <mitchman5506@gmail.com>
2015-07-18 20:17:39 +1000

    Updated comments

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -1,17 +1,19 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-#TODO Convert to reusable functions and objects
-def getLinks():
+
+# Downloads and logs all <a> tags into a text file
+def logLinks():
 	website = urllib2.urlopen(raw_input('Enter website url: '))
 	soup = BeautifulSoup(website.read())
 
 	log = open('log.txt', 'ab')
 
+	# Loop to write each tag to file
 	for link in soup.find_all('a'):
 		log.write(str(link.get('href')))
 		log.write('\n')
 		print (link.get('href'))
 	log.close()
 
-getLinks()
+logLinks()
