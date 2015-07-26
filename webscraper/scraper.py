171463a2917fb54b2c8be37249b0c3253f01c8c1
Mitch Rule <mitchman5506@gmail.com>
2015-07-26 21:52:23 +1000

    Worked on using CSV

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -9,18 +9,22 @@ import csv
 def logLinks():
 	connectBuilder = poolmanager.PoolManager()
 	# Take website url and name
-	website = connectBuilder.urlopen('GET', (input('Enter website url: ')))
+	website     = connectBuilder.urlopen('GET', (input('Enter website url: ')))
 	websiteName = input('What is the name of this website: ')
-	soup = BeautifulSoup(website.read(), "html.parser")
-	
-	# Open log txt file
-	log = open(websiteName + '.txt', 'a')
+	soup        = BeautifulSoup(website.read(), 'html.parser')
 
-	# Loop to write each tag to file
-	for link in soup.find_all('a'):
-		log.write(str(link.get('href')))
-		log.write('\n')
-		print (link.get('href'))
-	log.close()
+	csvfile = open(websiteName + '.csv', 'w')
+	writer = csv.writer(csvfile)
+
+	# Open log txt file
+	urls = []
+
+	for url in soup.findAll('a', href=re.compile('5'))
+		print url['href']
+
+	for url in zip(urls):
+		writer.writerow([url])
+
+	csvfile.close()
 
 logLinks()
